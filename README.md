acca586dcaf8a9af0f660451352af9c4b8bd88d0
nshoofy <Ck@123456789>
2022-04-07 01:48:09 +0300

    adding readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# rapid7-testsuite
-this script will test the GIST API requests options
+# Docker image rapid7-testsuite
+==========================
+this image will test the GIST API requests options 
+
+
+
+## Quick start
+1. Start a Docker container with your github personal token 
+
+```bash
+docker run -p 31031:31031 -e utoken='yourToken' nahedsh/testsuite-gist
+```
+
+2. go to http://localhost:31031 to see the allure report for all the testcases 
+
+3. That's it! 
--- conftest.py
+++ conftest.py
@@ -1,6 +1,17 @@
+################################ pytest_addoption ################################################
+"""
+        this is a setup method will parse the --token argument from the cmd line and append it to 
+        metafunc 
+"""
 def pytest_addoption(parser):
     parser.addoption('--token', action='append', help='token that will be used for testing')
 
+################################ pytest_generate_tests ###########################################
+"""
+        this is a setup method that will read the metafunc option and search for a spacific 
+        argumnet in our case is the 'token' and then it will load it to token variable as 
+        a global var
+"""
 def pytest_generate_tests(metafunc):
     try: 
         cmdtoken  = metafunc.config.getoption("token")
